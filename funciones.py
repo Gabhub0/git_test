4d49bdb8de4fd9fea4c4b715708d6f063d37bee1
Gabriel Mansilla <gabriel001mansilla@gmail.com>
2023-09-26 07:58:47 -0300

    nuevo archivo de funciones

---
+++ funciones.py
@@ -0,0 +1,7 @@
+def funciones():
+    for dato in datos:
+        if dato == usuario, password:
+            print('Bienvenido!')
+        else:
+            print('Datos incorrectos. Por favor, ingrese nuevamente')
+
